b1179b30728e89499957ccb64e72a00a956728dc
Yessica Mariesta <114063729+yessicamariesta@users.noreply.github.com>
2023-01-08 18:28:26 +0700

    Add files via upload

---
+++ src/pages/StockOverviewPage.js
@@ -0,0 +1,14 @@
+import { AutoComplete } from '../components/AutoComplete'
+import { StockList } from '../components/StockList'
+import trading from '../images/Trading.png'
+export const StockOverviewPage = () => {
+  return (
+    <div>
+      <div className='text-center'>
+        <img src={trading} />
+      </div>
+      <AutoComplete />
+      <StockList />
+    </div>
+  )
+}
